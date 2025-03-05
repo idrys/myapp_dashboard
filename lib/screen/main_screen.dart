dffa202ade2c41b6f1cdb553d2204303ef2cf9ac
idrys <idrys@1sw.pl>
2025-03-05 04:33:32 +0000

    Strona glowna Poczatek nowego widgetu

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,6 +1,7 @@
+import 'package:dashboard_workout/constants.dart';
 import 'package:dashboard_workout/screen/side_main.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -16,10 +17,54 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
         body: Row(
       children: [
-          const SideMain(),
+        const Expanded(
+          child: SideMain(),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
         Expanded(
-          flex: 1,
-          child: Container(),
+          flex: 4,
+          child: Container(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                        child: Container(
+                      height: 160,
+                      width: 250,
+                      decoration: const BoxDecoration(
+                        color: primaryColor,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                'yoga',
+                                style: GoogleFonts.ubuntu(
+                                  color: Colors.white, 
+                                  fontSize: 14, fontWeight: 
+                                  FontWeight.w600,
+                                  ),
+                              ),
+                              const SizedBox(
+                                height: 10, 
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ))
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ],
     ));
